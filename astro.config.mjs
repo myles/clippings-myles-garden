564c4a99a6cfc807fe3f3e548c8be6b236db31bd
Myles Braithwaite <myles@myles.garden>
2026-05-27 13:56:51 -0400

    feat(config): allows site to be changed with env

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import config from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://clippings.myles.garden",
+  site: config.CLIPPINGS_SITE,
 
   image: {
     domains: ["images.are.na"],
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 
 export default {
+  CLIPPINGS_SITE:
+    process.env.CLIPPINGS_SITE ?? "https://clippings.myles.garden",
   ARENA_API_KEY: process.env.CLIPPINGS_ARENA_API_KEY ?? "",
   ARENA_CHANNEL_ID: process.env.CLIPPINGS_ARENA_CHANNEL_ID!,
 };
