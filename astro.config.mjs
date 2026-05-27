d247007c56e79a7ddf3174fa7056aaba5094d74c
Myles Braithwaite <myles@myles.garden>
2026-05-27 13:46:30 -0400

    feat(deploy): adds the vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://clippings.myles.garden",
+
   image: {
     domains: ["images.are.na"],
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
