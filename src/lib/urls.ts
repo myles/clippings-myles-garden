8aebe7f243ddde40f07179feace3131d9e5efccd
Myles Braithwaite <myles@myles.garden>
2026-05-28 17:20:11 -0400

    feat(blocks): adds addtional metadata

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -3,6 +3,7 @@ export default {
     detail: (blockId: string | number) => `/${blockId}`,
   },
   external: {
+    user: (userId: string) => `https://www.are.na/${userId}`,
     block: (blockId: string | number) => `https://www.are.na/block/${blockId}`,
   },
 };
