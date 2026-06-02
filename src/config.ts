25b430a12b8022ef5da7ffb118f4bd78aa75f12e
Myles Braithwaite <myles@myles.garden>
2026-06-02 15:25:56 -0400

    chore(types): adds some typing to the config

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ import "dotenv/config";
 export default {
   SITE_URL: process.env.CLIPPINGS_SITE_URL ?? "https://clippings.myles.garden",
   ARENA_API_KEY: process.env.CLIPPINGS_ARENA_API_KEY ?? "",
-  ARENA_CHANNEL_ID: process.env.CLIPPINGS_ARENA_CHANNEL_ID!,
-};
+  ARENA_CHANNEL_ID: process.env.CLIPPINGS_ARENA_CHANNEL_ID ?? "",
+} as Record<string, string>;
