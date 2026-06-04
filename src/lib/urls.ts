f083e0d843cc2ca7f2651dca6b7aec88f1998d01
Myles Braithwaite <myles@myles.garden>
2026-06-04 15:13:56 -0400

    chore: just a bit of cleanup

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -1,9 +1,14 @@
 export default {
+  index: () => `/`,
+
   block: {
     detail: (blockId: string | number) => `/${blockId}`,
   },
+
   external: {
     user: (userId: string) => `https://www.are.na/${userId}`,
     block: (blockId: string | number) => `https://www.are.na/block/${blockId}`,
+    channel: (channelId: string | number) =>
+      `https://www.are.na/channel/${channelId}`,
   },
 };
